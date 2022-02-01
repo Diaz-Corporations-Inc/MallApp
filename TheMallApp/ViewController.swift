a5d8d8c10c98545a4f1466670098188dc9311124
devmspl <developer.mspl@gmail.com>
2022-02-01 15:46:32 +0530

    first commmit

---
+++ TheMallApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMallApp
+//
+//  Created by mac on 01/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hello")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
